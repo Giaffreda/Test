7a2d23d11d9b5849899f0c53a742c2f8313353c0
Giaffreda <43539325+Giaffreda@users.noreply.github.com>
2021-12-29 17:00:11 +0100

    Add files via upload

---
+++ src/main/java/Test/testsimple/App.java
@@ -0,0 +1,13 @@
+package Test.testsimple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
