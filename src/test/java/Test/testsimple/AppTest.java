4da57f73cfb18011ccb54634362211b28ee49a57
Giaffreda <43539325+Giaffreda@users.noreply.github.com>
2021-12-29 11:09:17 +0100

    Add files via upload

--- src/test/java/Test/testsimple/AppTest.java
+++ src/test/java/Test/testsimple/AppTest.java
@@ -1,12 +1,11 @@
 package Test.testsimple;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
- */
+ 
 public class AppTest 
     extends TestCase
 {
@@ -14,7 +13,7 @@ public class AppTest
      * Create the test case
      *
      * @param testName name of the test case
-     */
+     
     public AppTest( String testName )
     {
         super( testName );
@@ -22,7 +21,7 @@ public class AppTest
 
     /**
      * @return the suite of tests being tested
-     */
+     
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
@@ -30,9 +29,9 @@ public class AppTest
 
     /**
      * Rigourous Test :-)
-     */
+     
     public void testApp()
     {
         assertTrue( true );
-    }
+    }*/
 }
