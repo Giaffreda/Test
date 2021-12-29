58f5f7139dc35d000ec29a51a89d3a0d60548ce0
Giaffreda <43539325+Giaffreda@users.noreply.github.com>
2021-12-29 11:14:06 +0100

    Update AppTest.java

--- src/test/java/Test/testsimple/AppTest.java
+++ src/test/java/Test/testsimple/AppTest.java
@@ -3,35 +3,5 @@ package Test.testsimple;
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- 
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     
-    public void testApp()
-    {
-        assertTrue( true );
-    }*/
 }
