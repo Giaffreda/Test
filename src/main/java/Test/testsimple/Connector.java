c6719c93c5a951bf92c8ad770a0df71a0668a8ab
Giaffreda <43539325+Giaffreda@users.noreply.github.com>
2022-01-22 16:39:26 +0100

    Add files via upload

--- src/main/java/Test/testsimple/Connector.java
+++ src/main/java/Test/testsimple/Connector.java
@@ -62,7 +62,7 @@ public class Connector {
 				//_dht.put(Number160.createHash(name)).data(new Data( peers_on_topic=(new HashSet<PeerAddress>()))).start().awaitUninterruptibly();
 				peers_on_topic.add(_dht.peer().peerAddress());
 				
-				_dht.put(Number160.createHash(nickName)).data(new Data(peers_on_topic)).start().awaitUninterruptibly();
+				//_dht.put(Number160.createHash(name)).data(new Data(peers_on_topic)).start().awaitUninterruptibly();
 				/*
 				 *  _dht.put(Number160.createHash(name)).data(new Data( peers_on_topic=(new HashSet<PeerAddress>()))).start().awaitUninterruptibly();
 	        peers_on_topic.add(_dht.peer().peerAddress());
