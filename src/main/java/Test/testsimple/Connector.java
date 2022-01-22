ce9c6b66f0b7ad47988ea15958e010887d93650e
Giaffreda <43539325+Giaffreda@users.noreply.github.com>
2022-01-22 17:00:15 +0100

    Add files via upload

--- src/main/java/Test/testsimple/Connector.java
+++ src/main/java/Test/testsimple/Connector.java
@@ -75,7 +75,9 @@ public class Connector {
 					FutureDirect futureDirect = _dht.peer().sendDirect(peer).object(test).start();
 					futureDirect.awaitUninterruptibly();
 				}
-				//_dht.put(Number160.createHash(nickName)).data(new Data( peers_on_topic=(new HashSet<PeerAddress>()))).start().awaitUninterruptibly();
+				_dht.put(Number160.createHash(nickName)).data(new Data( peers_on_topic=(new HashSet<PeerAddress>()))).start().awaitUninterruptibly();
+				peers_on_topic.add(_dht.peer().peerAddress());
+
 				_dht.put(Number160.createHash(nickName)).data(new Data( peers_on_topic)).start().awaitUninterruptibly();
 
 				//_dht.put(Number160.createHash(nickName)).data(new Data( peers_on_topic)).start().awaitUninterruptibly();
