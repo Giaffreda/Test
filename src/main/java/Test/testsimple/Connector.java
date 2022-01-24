2d0e3dc750f70d25e0d3363cb1f3cd9d3565fec2
Giaffreda <43539325+Giaffreda@users.noreply.github.com>
2022-01-24 18:12:47 +0100

    Add files via upload

--- src/main/java/Test/testsimple/Connector.java
+++ src/main/java/Test/testsimple/Connector.java
@@ -111,6 +111,7 @@ public class Connector {
 				test=new App(profilekey, peerId, nickName);
 				//_dht.put(Number160.createHash(nickName)).data(new Data(new HashSet<PeerAddress>())).start().awaitUninterruptibly();
 				//peers_on_topic.add(_dht.peer().peerAddress());
+				_dht.put(Number160.createHash(nickName)).data(new Data(new HashSet<PeerAddress>())).start().awaitUninterruptibly();
 				System.out.println("nick name per send di test ="+test.getNickname());
 				test.setMytype(App.type.friends);
 				for(PeerAddress peer:peers_on_topic){
@@ -118,7 +119,7 @@ public class Connector {
 					FutureDirect futureDirect = _dht.peer().sendDirect(peer).object(test).start();
 					futureDirect.awaitUninterruptibly();
 				}
-				_dht.put(Number160.createHash(nickName)).data(new Data(new HashSet<PeerAddress>())).start().awaitUninterruptibly();
+				//_dht.put(Number160.createHash(nickName)).data(new Data(new HashSet<PeerAddress>())).start().awaitUninterruptibly();
 			}
 			}catch (Exception e) {
 				// TODO: handle exception
