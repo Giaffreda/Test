23e08b7df6798e079a826606e827b88d3ae0af36
Giaffreda <43539325+Giaffreda@users.noreply.github.com>
2022-01-24 15:59:14 +0100

    Add files via upload

--- src/main/java/Test/testsimple/Connector.java
+++ src/main/java/Test/testsimple/Connector.java
@@ -130,7 +130,7 @@ public class Connector {
 			if (futureGet.isSuccess() && futureGet.isEmpty()) {
 				HashSet<PeerAddress> peers_on_topic;
 	        _dht.put(Number160.createHash(name)).data(new Data( peers_on_topic=(new HashSet<PeerAddress>()))).start().awaitUninterruptibly();
-	        // peers_on_topic.add(_dht.peer().peerAddress());
+	        peers_on_topic.add(_dht.peer().peerAddress());
 	        //_dht.put(Number160.createHash(name)).data(new Data(peers_on_topic)).start().awaitUninterruptibly();
 	        System.out.print("put test");
 			}
